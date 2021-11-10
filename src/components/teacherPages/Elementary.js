ac9693435a7923b448bf452a804371780b2a7af7
Gregory Yao <58223977+gyao6242@users.noreply.github.com>
2021-11-09 19:09:16 -0500

    Update and rename K-6.js to Elementary.js

--- src/components/teacherPages/Elementary.js
+++ src/components/teacherPages/Elementary.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 
-function K-6() {
+function Elementary() {
     return (
         <>
-        <div>K-6</div>
+        <div>Elementary</div>
         <Link to='/home'>Home</Link>
         <Link to='/teacher'>Teacher</Link>
         </>
     )
 }
 
-export default K-6;
+export default Elementary;
