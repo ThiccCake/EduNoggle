631bf9c512aaf3f51a4216fa3c2d8398d772f1ae
Sahbello <65436663+Sahbello@users.noreply.github.com>
2021-11-11 16:06:34 -0500

    Uploading Edunoggle Code so far

---
+++ src/component/About/About.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div className="container">
+            <h1 className="p-2 mt-2"><b>About</b></h1>
+            <p className="text-justify">
+            COVID-19 has shifted the educational landscape. Schools and Parents will benefit from a new form of partnership this is where EduNoggle will come in to support all community members.
+                    
+                     Every child needs a champion and I was fortunate to have 2 cheerleaders. Now it is my turn to share what I have learned on this journey. 
+                      
+                        The tools you will find in EduNoggle will provide you insight to be the champion for your student children and community. 
+                         
+                            Yes, the gift of lifelong learning will be supported with the various tools in the subscription to EduNoggle. As part of the subscription, a family will have various access to Pk -12 learning tools. 
+                            
+                                We also provide 1:1 services for families who need support in advocating for students who have diverse learning needs. 
+                            
+                                     Besides, we offer consultation for school districts that need a thought partner to ensure all students will have learning opportunities with the focus of inclusion for all learners. 
+                            
+                                        COVID-19 has accelerated the importance of student independence to the earlier grades. Let's go on this journey together!
+            </p>
+        </div>
+    );
+};
+
+export default About;
